4a7c907b0aea6a15911a08da21912aef4f4a1fc3
rhnurr02 <rhnurrahman.02@gmail.com>
2025-10-07 12:02:33 -0400

    Working on android compatability. unfinished and messy at the moment

---
+++ android/app/src/main/java/io/github/rhnurr02/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.rhnurr02.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
